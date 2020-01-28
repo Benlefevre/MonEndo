636431fb5c3214b2c21d228c50161c11b61e02e6
Benlefevre <benoit_lefevre@orange.fr>
2020-01-28 19:06:34 +0100

    Creating the login activity and adding the Firebase dependencies to use FirebaseAuth. Setting the configuration for Auth into Firebase Console.

--- app/src/main/java/com/benlefevre/monendo/ui/controllers/activities/MainActivity.kt
+++ app/src/main/java/com/benlefevre/monendo/ui/controllers/activities/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.benlefevre.monendo.ui.controllers.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.benlefevre.monendo.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        } else {
+            setContentView(R.layout.activity_main)
+        }
     }
 }
---
+++ app/src/main/java/com/benlefevre/monendo/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.benlefevre.monendo.utils
+
+const val RC_SIGN_IN = 1
