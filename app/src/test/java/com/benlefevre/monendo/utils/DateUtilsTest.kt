c99636d8d20fb5d8d2e11c3265ce465bf922dceb
Benlefevre <benoit_lefevre@orange.fr>
2020-03-12 16:18:17 +0100

    Correction of the failed test in DateUtilsTest.

--- app/src/test/java/com/benlefevre/monendo/utils/DateUtilsTest.kt
+++ app/src/test/java/com/benlefevre/monendo/utils/DateUtilsTest.kt
@@ -7,40 +7,41 @@ import org.junit.runners.JUnit4
 import java.util.*
 
 @RunWith(JUnit4::class)
-class DateUtilsTest{
+class DateUtilsTest {
 
     @Test
-    fun formatDateWithoutYear_success_correctDataReturned(){
-        val date = with(Calendar.getInstance()){
-            set(Calendar.DAY_OF_MONTH,1)
-            set(Calendar.MONTH,0)
+    fun formatDateWithoutYear_success_correctDataReturned() {
+        val date = with(Calendar.getInstance()) {
+            set(Calendar.DAY_OF_MONTH, 1)
+            set(Calendar.MONTH, 0)
             time
         }
         assertEquals("01/01", formatDateWithoutYear(date))
     }
 
     @Test
-    fun formatDateWithYear_success_correctDataReturned(){
-        val date = with(Calendar.getInstance()){
-            set(Calendar.DAY_OF_YEAR,1)
-            set(Calendar.MONTH,0)
-            set(Calendar.YEAR,2020)
+    fun formatDateWithYear_success_correctDataReturned() {
+        val date = with(Calendar.getInstance()) {
+            set(Calendar.DAY_OF_YEAR, 1)
+            set(Calendar.MONTH, 0)
+            set(Calendar.YEAR, 2020)
             time
         }
         assertEquals("01/01/20", formatDateWithYear(date))
     }
 
     @Test
-    fun parseStringInDate_success_correctDataReturned(){
+    fun parseStringInDate_success_correctDataReturned() {
         val date = Date(1583794800000)
-        assertEquals(date, parseStringInDate("10/03/20"))
+        if (TimeZone.getDefault() == TimeZone.getTimeZone("Europe/Paris"))
+            assertEquals(date, parseStringInDate("10/03/20"))
     }
 
     @Test
     fun formatTime_success_correctDataReturned() {
-        val date = with(Calendar.getInstance()){
-            set(Calendar.HOUR_OF_DAY,12)
-            set(Calendar.MINUTE,0)
+        val date = with(Calendar.getInstance()) {
+            set(Calendar.HOUR_OF_DAY, 12)
+            set(Calendar.MINUTE, 0)
             time
         }
         assertEquals("12:00", formatTime(date))
